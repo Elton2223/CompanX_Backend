ccc4294f12bdd08bb71a83f86047f9985d19398b
Khethani Savhasa <122065132+Elton2223@users.noreply.github.com>
2025-02-18 23:43:41 +0200

    updated modules

--- models/user.js
+++ models/user.js
@@ -1,31 +1,53 @@
-module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define("User", {
-        userId: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        firstName: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-	surname: {
-	    type: DataTypes.STRING,
-            allowNull: false
-	},
-	email: {
-	    type: DataTypes.STRING,
-            allowNull: false
-	},
-	dept: {
-	    type: DataTypes.STRING,
-            allowNull: false
-	},
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    });
-
-    return User;
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('user', {
+    userId: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    firstName: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    surname: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(500),
+      allowNull: false
+    },
+    dept: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING(500),
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    tableName: 'user',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "userId" },
+        ]
+      },
+      {
+        name: "userId_UNIQUE",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "userId" },
+        ]
+      },
+    ]
+  });
 };
